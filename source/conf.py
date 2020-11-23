ef04e3a381ecab803ecabe14a9318d12dfcc119d
core-man <coreman.seism@gmail.com>
2020-11-23 15:53:30 +0800

    add an example of plot sac data

--- source/conf.py
+++ source/conf.py
@@ -26,12 +26,15 @@ import os
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+highlight_language = 'bash'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.githubpages',
+    'sphinx_gmt.gmtplot'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
